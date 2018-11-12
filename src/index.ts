a154501a91e41d944bb772c05ad4925f88950297
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-12 21:52:21 +0000

    playground

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-(() => console.log("Hi!"))();
+import * as signtext from "signtext";
+
+(function () {
+  function getPlayground() {
+    return ({
+      input_javascript: playground_input_js.value,
+      input_signtext: playground_input_st.value,
+      namespace: eval(`(${playground_input_js.value})`)
+    });
+  }
+
+  const playground_input_st = document.getElementById("playground-st-code") as HTMLTextAreaElement;
+  const playground_input_js = document.getElementById("playground-js-code") as HTMLTextAreaElement;
+  const playground_output = document.getElementById("playground-output") as HTMLTextAreaElement;
+  const rt = new signtext.Runtime();
+
+  document.getElementById("playground").addEventListener("submit", function (ev) {
+    ev.preventDefault();
+
+    const pg = getPlayground();
+    rt.eval(pg.input_signtext, pg.namespace)
+        .catch((err) => err)
+        .then((x) => playground_output.innerText = x);
+
+    return false;
+  });
+})();
